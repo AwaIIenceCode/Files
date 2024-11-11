4d47be5be509470de47d6f99c854e27bbd380acc
Oleh Skrypnyk-Zakrevskiy <awallencecode@gmail.com>
2024-11-11 20:57:55 +0200

    ex_6 is done

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-search_word = input("Enter the word to count: ")
+find_word = input("Enter the word to find: ")
+replace_word = input("Enter the word to replace it with: ")
 
 with open('input.txt', 'r', encoding='utf-8') as file:
-    words = file.read().split()
-    count = 0
+    content = file.read()
 
-for word in words:
-    if word == search_word:
-        count += 1
+new_content = content.replace(find_word, replace_word)
 
-print(f"The word '{search_word}' appears {count} times.")
+with open('output.txt', 'w', encoding='utf-8') as outfile:
+    outfile.write(new_content)
+
+print(f"The word '{find_word}' has been replaced with '{replace_word}' in the output file.")
