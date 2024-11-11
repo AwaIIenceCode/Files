4f37680318c4f61826533d73e2f50e0e620aa461
Oleh Skrypnyk-Zakrevskiy <awallencecode@gmail.com>
2024-11-11 19:56:15 +0200

    ex_10 is done

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-#var_1
 with open('input.txt', 'r', encoding='utf-8') as infile:
-    content = infile.read()
-    print(f"Number of characters in a file: {len(content)}")
-
-#var_2
-with open('input.txt', 'r', encoding='utf-8') as infile:
-    total_characters = 0
-
-    for line in infile:
-        total_characters += len(line)
-
-print(f"Total number of characters: {total_characters}")
+    lines = infile.readlines()
+    print(f"Number of rows in a file: {len(lines)}")
