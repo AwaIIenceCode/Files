13fd70b47a59ac46014532189223c353e45c02c0
Oleh Skrypnyk-Zakrevskiy <awallencecode@gmail.com>
2024-11-11 19:33:12 +0200

    ex_4 is done

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 with open('input.txt', 'r', encoding='utf-8') as infile:
     lines = infile.readlines()
+
+inserted = False
 with open('output.txt', 'w', encoding='utf-8') as outfile:
-    for line in reversed(lines):
+    for line in lines:
         outfile.write(line)
+        if ',' not in line and not inserted:
+            outfile.write('************\n')
+            inserted = True
+
+if not inserted:
+    with open('output.txt', 'a', encoding='utf-8') as outfile:
+        outfile.write('************\n')
