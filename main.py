1e7f053b51b9a8b774c0e33f26e3211d84864418
Oleh Skrypnyk-Zakrevskiy <awallencecode@gmail.com>
2024-11-11 20:52:42 +0200

    ex_4 is done

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 with open('input.txt', 'r', encoding='utf-8') as file:
     lines = file.readlines()
+    max_length = 0
 
-with open('output.txt', 'w', encoding='utf-8') as outfile:
-    outfile.writelines(lines[:-1])
+for line in lines:
+    line = line.strip()
+    line_length = len(line)
+
+    if line_length > max_length:
+        max_length = line_length
+
+print(f"The length of the longest line is: {max_length}")
