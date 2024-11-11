49ceb0749b290c57dd0d2f360b4efcf1fcc73d9a
Oleh Skrypnyk-Zakrevskiy <awallencecode@gmail.com>
2024-11-11 19:39:07 +0200

    ex_6 is done

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-symbol = input("Enter the latter: ").lower()
-count = 0
-
 with open('input.txt', 'r', encoding='utf-8') as infile:
-    words = infile.read().split()
+    content = infile.read().replace('*', 'TEMP').replace('&', '*').replace('TEMP', '&')
 
-    for word in words:
-        if word.lower().startswith(symbol):
-            count += 1
-
-print(f"The number of words that begin with: '{symbol}': {count}")
+with open('output.txt', 'w', encoding='utf-8') as outfile:
+    outfile.write(content)
