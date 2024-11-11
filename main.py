9c24ad4cd0b8c5e5b26c801e9ad494ed5fc3811f
Oleh Skrypnyk-Zakrevskiy <awallencecode@gmail.com>
2024-11-11 19:32:06 +0200

    ex_3 is done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 with open('input.txt', 'r', encoding='utf-8') as infile:
-    with open('output.txt', 'w', encoding='utf-8') as outfile:
-        for line in infile:
-            outfile.write(line)
+    lines = infile.readlines()
+with open('output.txt', 'w', encoding='utf-8') as outfile:
+    for line in reversed(lines):
+        outfile.write(line)
